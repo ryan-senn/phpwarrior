7a69ac6b3a6bffe4031e2db9f62b3b61d21e12bc
Ryan Senn <ryan.senn.au@gmail.com>
2014-01-19 04:51:21 -0800

    update config

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'qKDpCbjnW1bn7JwJ245YUNTdWstvHn9u',
 
 	/*
 	|--------------------------------------------------------------------------
