72bd6788bf804aade82bdc30adb0be6c2b33c121
Ryan-Senn <ryan.senn.au@gmail.com>
2014-02-18 16:23:31 +1000

    fix Feel skill

--- app/services/game/skills/Feel.php
+++ app/services/game/skills/Feel.php
@@ -9,28 +9,6 @@ class Feel extends Skill
 	
 	public function execute($direction = 'forward')
 	{
-		$this->unit->addEvent('walks '. $direction);
-
-		$space = $this->unit->getSpace($direction);
-
-		if($space->isEmpty())
-		{
-			$this->unit->getPosition()->move($direction);
-			return;
-		}
-
-		if($space->isWall())
-		{
-			$this->unit->addEvent('bumps into a wall');
-			return;
-		}
-
-		if($space->isEnnemy())
-		{
-			$this->unit->addEvent('bumps into '. $this->unit->getSpace($direction)->getUnit());
-			return;
-		}
-
-		throw new \Exception('walk skill doesnt know what to do');
+		return $this->unit->getSpace($direction);
 	}
 }
